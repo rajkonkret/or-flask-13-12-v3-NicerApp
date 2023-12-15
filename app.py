d3bdb4d309a96ea488ca7b0f660b27d011cddd1a
rajkonkret <rajkonkret660@gmail.com>
2023-12-15 11:49:20 +0100

    3 - add create user admin

--- app.py
+++ app.py
@@ -80,12 +80,33 @@ class UserPass:
         return pwdhash == stored_password
 
     def get_random_user_password(self):
-        random_user = ''.join(random.choice(string.ascii_lowercase)for i in range(3))
+        random_user = ''.join(random.choice(string.ascii_lowercase) for i in range(3))
         self.user = random_user
 
         password_characters = string.ascii_letters
         random_password = ''.join(random.choice(password_characters) for i in range(3))
         self.password = random_password
+
+
+@app.route('/init_app')
+def init_app():
+    db = get_db()
+    sql_statement = 'select count(*) as cnt from users where is_active and is_admin;'
+    cur = db.execute(sql_statement)
+    active_admins = cur.fetchone()
+
+    if active_admins != None and active_admins['cnt'] > 0:
+        flash("Application is already set-up. Nothing to do")
+        return redirect(url_for('index'))
+
+    user_pass = UserPass()
+    user_pass.get_random_user_password()
+    db.execute('''insert into users(name, email, password, is_active, is_admin)
+    values(?,?,?,True, True);''',
+               [user_pass.user, 'noone@nowhere.no', user_pass.hash_password()])
+    db.commit()
+    flash('User {} with password {} has been added'.format(user_pass.user, user_pass.password))
+    return redirect(url_for('index'))
 
 
 @app.route('/')
